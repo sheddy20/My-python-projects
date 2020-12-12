7317129451b3ffe36adb6064de1af5e6eacc1ba0
sheddy20 <shedrackejeh458@gmail.com>
2020-12-12 11:51:14 +0100

    git commit -m 'done'

---
+++ Condition.py
@@ -0,0 +1,32 @@
+# Not comparison
+userName = 'marks'
+
+isLoggedin = True
+
+if not isLoggedin:
+    print('please Log in')
+
+else:
+    print('Welcome Back John')
+
+# And Comparison
+first_name = 'Elon'
+last_name = 'Musk'
+
+if first_name == 'Elon' and last_name == 'Musk':
+    print('his name is Elon Musk')
+
+else:
+    print('Not Elon Musk')
+
+
+# Or Comparison
+age = 45
+spouse = "Justine Musk"
+isDivorce = False
+
+if age == 450 or spouse == 'Mustine Musk' or isDivorce:
+    print('True details is complete')
+
+else:
+    print('Invalid Details')
